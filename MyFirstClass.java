2fa703870c5dee13564b3082aca38d7977373bb0
ConaxFox <47067110+ConaxFox@users.noreply.github.com>
2019-01-26 16:07:03 -0500

    Add files via upload

---
+++ MyFirstClass.java
@@ -0,0 +1,16 @@
+/* The first class we have programmed in EECS */
+public class MyFirstClass extends Object {
+  
+  /* some fields */
+  public double factor;
+  
+  /* a method to compute the subtraction of two integers */
+  public int subtract(int x, int y) {
+    return x - y;
+  }
+  
+  /* multiply a value by the factor*/
+  public double scale(double x) {
+    return x * factor;
+  }
+}
